f3c0eed858b2520a6dff9f3cddecce791eac46bd
hackersubhash <Pandeysu@xuber.com>
2016-05-20 15:24:56 +0530

    Remove

--- MvcApplication1/Controllers/HomeController.cs
+++ MvcApplication1/Controllers/HomeController.cs
@@ -8,17 +8,17 @@ namespace MvcApplication1.Controllers
 {
     public class HomeController : Controller
     {
-        EF_TestEntities con = new EF_TestEntities();
+        // EF_TestEntities con = new EF_TestEntities();
         public ActionResult Index()
         {
-            ViewBag.Message = con.Messages.First().Message1;
+            // ViewBag.Message = con.Messages.First().Message1;
 
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = con.Messages.Last().Message1;
+            // ViewBag.Message = con.Messages.Last().Message1;
 
             return View();
         }
