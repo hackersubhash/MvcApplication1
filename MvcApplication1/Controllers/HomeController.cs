e26d6b66ffafe1c6b0befc2196f8d444a785ffc5
hackersubhash <Pandeysu@xuber.com>
2016-05-20 15:10:38 +0530

    a
    a

---
+++ MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        EF_TestEntities con = new EF_TestEntities();
+        public ActionResult Index()
+        {
+            ViewBag.Message = con.Messages.First().Message1;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = con.Messages.Last().Message1;
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
